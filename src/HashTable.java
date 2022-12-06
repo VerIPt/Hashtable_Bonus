f3d7c11889821c848c4d4d52b312b234a7f69aaf
verit <verit@posteo.de>
2022-12-07 00:50:50 +0100

    final, clean but without tests -> refactor at 80%

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,190 +1,185 @@
 import java.util.LinkedList;
 
-    /*
-     * @author Gwarda(942437)
-     * @author Maushart(939781)
-     */
+/*
+ * @author Gwarda(942437)
+ * @author Maushart(939781)
+ */
 
-    // Inner class for Key-Value-Pairs with attributes key and String
-    class KeyValuePair
-    {
-        int key;
-        String value;
+// Inner class for Key-Value-Pairs with attributes key and String
+class KeyValuePair {
+    int key;
+    String value;
 
-        //Getter
-        public int getKey() {
-            return key;
-        }
-        public String getValue() {
-            return value;
-        }
-
-        // Constructor
-        public KeyValuePair(int key, String value)
-        {
-            this.key = key;
-            this.value = value;
-        }
+    //Getter
+    public int getKey() {
+        return key;
     }
-    /*
-        Class hashtable with linked-lists as an array for implementation
-        with variable, chose able size as integer
+
+    public String getValue() {
+        return value;
+    }
+
+    // Constructor
+    public KeyValuePair(int key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+/*
+    Class hashtable with linked-lists as an array for implementation
+    with variable, chose able size as an integer
+ */
+public class HashTable implements IntStringMap {
+    private int size;
+    private int capacy;
+    private LinkedList<KeyValuePair>[] hashtable;
+
+    // Getter
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacy() {
+        return capacy;
+    }
+
+    // Constructor hashtable with size and array of linked-list with KeyValuePairs
+    @SuppressWarnings("unchecked")
+    public HashTable(int size) {
+        this.size = size;
+        hashtable = (LinkedList<KeyValuePair>[]) new LinkedList[size];
+    }
+
+    // Hash-function to calculate the positive hash for Index (absolute -> Math.abs)
+    private int hashFunction(Integer key) {
+        return Math.abs(key.hashCode() % size);
+    }
+
+
+    /**
+     * Method for factorisation the Hashtable to more capacy
+     * @param old       = Linked list of KeyValuePairs -> hashtable
+     * @param oldCapacy = int with the capacy of the hashtable filled over a certain amount of %
+     * @return Linked list of KeyValuePairs
+     * HT = Hashtable
+     * Method to refactor the input HT to a new one with the size of the next given prime number from the
+     * old size. The method generates the new HT as an array of Linked-list and iterates with the nested
+     * for-loops through the old list to copy the KeyValuePairs with recalculation into the new HT.
      */
-    public class HashTable implements IntStringMap {
-        private int size;
-        private LinkedList<KeyValuePair>[] hashtable;
-
-        // Getter
-        public int getSize() {
-            return size;
-        }
-
-        // Constructor hashtable with size and array of linked-list with KeyValuePairs
+    private LinkedList<KeyValuePair>[] reFactorHashtable(LinkedList<KeyValuePair>[] old, int oldCapacy) {
         @SuppressWarnings("unchecked")
-        public HashTable(int size) {
-            this.size = size;
-            hashtable = (LinkedList<KeyValuePair>[]) new LinkedList[size];
-        }
-
-        // Hash-function to calculate the positive hash for Index (absolute -> Math.abs)
-        private int hashFunction(Integer key) {
-            return Math.abs(key.hashCode() % size);
-        }
-
-        /**
-         * Method for checking keys and removing them if found
-         * @param key = Integer
-         * @param index = int
-         * @return = removed Value or if not available null
-         */
-        private String checkAndRemove(Integer key, int index) {
-            String value = null;
-            LinkedList<KeyValuePair> list = hashtable[index];
-            //iterating through linked-list at index
-            for (int i = 0; i < list.size(); i++) {
-                KeyValuePair keyValuePair = list.get(i);
-                int keyIndex = keyValuePair.getKey();       // saves every key for checking
-                // checks for matching keys, and removes the Key-Value-pair -> returns removed value
-                if (key == keyIndex) {
-                    value = keyValuePair.getValue();
-                    list.remove(i);
-                    break;
+        LinkedList<KeyValuePair>[] newHashTable = new LinkedList[nextPrime(oldCapacy)];
+        for (int i = 0; i < old.length; i++) {
+            LinkedList<KeyValuePair> list = hashtable[i];
+            for (int j = 0; j <= old.length; j++) {
+                if (list != null) {
+                    if (list.size() == j)   // to avoid out-of-bounds-exception
+                        break;
+                    KeyValuePair kvp = list.get(j);
+                    int index = hashFunction(kvp.getKey());
+                    if (newHashTable[index] == null)              // to avoid null-pointer-exception
+                        newHashTable[index] = new LinkedList<>();
+                    LinkedList<KeyValuePair> newList = newHashTable[index];
+                    KeyValuePair add = new KeyValuePair(kvp.getKey(), kvp.getValue());
+                    newList.add(add);
                 }
             }
-            return value;
         }
-
-        /**
-         * {@link  IntStringMap#put(Integer, String)}
-         * Method for adding new Key-Value-pairs to the hashtable. With included function
-         * to avoid doubled keys, in that case the key is saved with the new given value
-         */
-        public String put(Integer key, String newValue) {
-            // determine new index with the hash function
-            int index = hashFunction(key);
-            int capacy = 0;
-            // checking capacy of hashTable
-            for (LinkedList<KeyValuePair> keyValuePairs : hashtable)
-                if (keyValuePairs != null) {
-                    capacy++;
-                }
-
-            if (capacy + 2 == hashtable.length) {
-                LinkedList<KeyValuePair>[] newHashTable;
-                newHashTable = reFactorHashtable(hashtable, hashtable.length);
-                hashtable = newHashTable;
-                System.out.println(size = newHashTable.length);
-
-            }
-            /*
-            In case the index in the array (hashtable) is null, there will be
-            a new empty linked-list created, in witch there can be new Key-Value-Objects
-            generated and saved
-            */
-            if (hashtable[index] == null)
-                hashtable[index] = new LinkedList<>();
-
-            //checks if the key already exists info @ checkAndRemove
-            String value = checkAndRemove(key, index);
-            // Generates and adds a new Key-Value-Pair to hashtable
-            LinkedList<KeyValuePair> list = hashtable[index];
-            KeyValuePair add = new KeyValuePair(key, newValue);
-            list.add(add);
-            System.out.println("Hinzugefügt bei key; "+key+" at index: "+index+" "+newValue);
-            System.out.println( "Alter Wert: "+ value);
-            return value;
-        }
-
-        /**{@link IntStringMap#get(Integer)}
-         * Method for getting a value, to a key. It checks if the index is null and goes through all linked-lists
-         * at the index in the hashtable if the index isn't null. Return value is a string of the value from the
-         * matching key, or null if not found
-         */
-        public String get(Integer key) {
-            int index = hashFunction(key);
-            if (hashtable[index] != null) {
-                // saves the linked-list at the index in the hashtable from the given Key-Value-pair
-                LinkedList<KeyValuePair> list = hashtable[index];
-                for (KeyValuePair keyValuePair : list) {        // iterates through linked-list
-                    int savedKey = keyValuePair.getKey();       // saves every key for checking
-                    // check for matching keys and returns value as String if found
-                    if (key == savedKey) {
-                        System.out.println("get from key: "+key+"at index: "+index+" "+keyValuePair.getValue());
-                        return keyValuePair.getValue();
-                    }
-                }
-            }
-            return null;
-        }
-
-        /**
-         * {@link IntStringMap#remove(Integer)}
-         * Method that uses {@link HashTable#checkAndRemove(Integer, int)}
-         * to remove objects from the hashtable, with return as String from removed value
-         * or null if not found
-         */
-        public String remove(Integer key) {
-            int index = hashFunction(key);
-            if (hashtable[index] != null) {
-                System.out.println(checkAndRemove(key, index));
-                return checkAndRemove(key,index);
-
-            }
-            return null;
-        }
-        // Bonus
-        private LinkedList<KeyValuePair>[] reFactorHashtable(LinkedList<KeyValuePair>[] old, int oldCapacy){
-            @SuppressWarnings("unchecked")
-            LinkedList<KeyValuePair>[] newHashTable = new LinkedList[nextPrime(oldCapacy)];
-            for (int i = 0; i < old.length; i ++){
-                LinkedList<KeyValuePair> list = hashtable[i];
-                for (int j = 0; j <= old.length; j++) {
-                    if (hashtable[i] != null) {
-                        if (list.size() == j)
-                            break;
-                        KeyValuePair kvp = list.get(j);
-                        int key = kvp.getKey();
-                        String value = kvp.getValue();
-                        if (newHashTable[hashFunction(kvp.getKey())] == null)
-                            newHashTable[hashFunction(kvp.getKey())] = new LinkedList<>();
-                        LinkedList<KeyValuePair> newList = newHashTable[hashFunction(kvp.getKey())];
-                        KeyValuePair add = new KeyValuePair(key, value);
-                        newList.add(add);
-                        System.out.println("Hinzugefügt bei key; "+key+" at index: "+hashFunction(kvp.getKey())+" "+value);
-                    }
-                }
-            }
-            return newHashTable;
-        }
-        private int nextPrime(int num) {
-            num++;
-            for (int i = 2; i < num; i++) {
-                if(num%i == 0) {
-                    num++;
-                    i=2;
-                }
-            }
-            return num;
-        }
+        return newHashTable;
     }
+    // method for calculating prime numbers
+    private int nextPrime(int num) {
+        num++;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                num++;
+                i = 2;
+            }
+        }
+        return num;
+    }
+
+    /**
+     * Method for checking keys and removing them if found
+     * @param key   = Integer
+     * @param index = int
+     * @return = removed Value or if not available null
+     * The method is iterating through the linked list at the calculated index in the array
+     * and compares every saved key with the new given key. The value of a matching key gets saved
+     * in a variable called "value" and the "KeyValuePair" removed from the list. If no match is
+     * the value given back is null
+     */
+    private String checkAndRemove(Integer key, int index) {
+        String value = null;
+        LinkedList<KeyValuePair> list = hashtable[index];
+        for (int i = 0; i < list.size(); i++) {
+            KeyValuePair keyValuePair = list.get(i);
+            int keyIndex = keyValuePair.getKey();
+            if (key == keyIndex) {
+                value = keyValuePair.getValue();
+                list.remove(i);
+                break;
+            }
+        }
+        return value;
+    }
+    // Interface-implementation:
+    /**
+     * {@link  IntStringMap#put(Integer, String)}
+     * Method for adding new Key-Value-pairs to the hashtable. With included function
+     * to avoid doubled keys, in that case the key is saved with the new given value
+     */
+    public String put(Integer key, String newValue) {
+        int index = hashFunction(key);
+        if ((100 / hashtable.length) * capacy >= 80) {                  // capacy-check at over 80 % -> refactor
+            hashtable = reFactorHashtable(hashtable, hashtable.length); //info @ reFactorHashtable
+            size = hashtable.length;
+        }
+        if (hashtable[index] == null) {     // to avoid null-pointer-exception
+            hashtable[index] = new LinkedList<>();
+            capacy++;
+        }
+        String value = checkAndRemove(key, index);        //checks if the key already exists info @ checkAndRemove
+        LinkedList<KeyValuePair> list = hashtable[index]; // Generates and adds a new Key-Value-Pair to hashtable
+        KeyValuePair add = new KeyValuePair(key, newValue);
+        list.add(add);
+        return value;
+    }
+
+    /**
+     * {@link IntStringMap#get(Integer)}
+     * Method for getting a value, to a key. It checks if the index isn't null and iterates through the
+     * linked-list at the index in the hashtable. Return value is a string of the value from the matching key,
+     * or null if not found.
+     */
+    public String get(Integer key) {
+        int index = hashFunction(key);
+        if (hashtable[index] != null) {
+            LinkedList<KeyValuePair> list = hashtable[index];   // Gets the linked list at the Index to compare keys
+            for (KeyValuePair keyValuePair : list) {
+                int savedKey = keyValuePair.getKey();
+                if (key == savedKey) {
+                    return keyValuePair.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * {@link IntStringMap#remove(Integer)}
+     * Method that uses {@link HashTable#checkAndRemove(Integer, int)}
+     * to remove objects from the hashtable, with return as String from removed value
+     * or null if not found
+     */
+    public String remove(Integer key) {
+        int index = hashFunction(key);
+        if (hashtable[index] != null) {
+            System.out.println(checkAndRemove(key, index));
+            return checkAndRemove(key, index);
+        }
+        return null;
+    }
+}
 
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,30 @@
+
 public class Main {
     public static void main(String[] args) {
         HashTable ht = new HashTable(11);
-        System.out.println("Add to key 12334: " + ht.put(12334, "lala luna"));
-        System.out.println("Add to key 98873: " + ht.put(98873, "Hugo Hafer "));
-        System.out.println("Add to key 98873: " + ht.put(98873, "Fina Fein ") + ht.get(98873));
-        System.out.println("Key 98873: " + ht.get(98873));
-        System.out.println("key 98873 removed: " + ht.remove(98873));
-        System.out.println("Key 98873: " + ht.get(98873));
-        System.out.println("Add to key 5: " + ht.put(5, "Fischers Fritz "));
-        System.out.println("Key 12334: " + ht.get(12334));
-        System.out.println("key 5: " + ht.get(5));
-        System.out.println("Add to key 12334: " + ht.put(12334, "lala luna"));
-        ht.get(98873);
-        ht.put(98873, "Ingo Igel ");
-        System.out.println(ht.getSize());
+        addingTests(ht);
 
         for (int i = 0; i <= 11; i++) {
             System.out.println("Add to key " + i + ": " + ht.put(i, Integer.toString(i)));
         }
+        System.out.println("Capacy : " + ht.getCapacy());
 
         System.out.println(ht.getSize());
+        addingTests(ht);
+
+        for (int i = 0; i <= 15; i++) {
+            System.out.println("Add to key " + i + ": " + ht.put(i, Integer.toString(i)));
+        }
+        System.out.println(ht.getSize());
+        System.out.println("Capacy : " + ht.getCapacy());
+        System.out.println();
+    }
+
+
+
+
+
+    private static void addingTests(HashTable ht) {
         System.out.println("Add to key 12334: " + ht.put(12334, "lala luna"));
         System.out.println("Add to key 98873: " + ht.put(98873, "Hugo Hafer "));
         System.out.println("Add to key 98873: " + ht.put(98873, "Fina Fein ") + ht.get(98873));
@@ -33,11 +38,6 @@ public class Main {
         ht.get(98873);
         ht.put(98873, "Ingo Igel ");
         System.out.println(ht.getSize());
-
-        for (int i = 0; i <= 15; i++) {
-            System.out.println("Add to key " + i + ": " + ht.put(i, Integer.toString(i)));
-        }
-
-
+        System.out.println("Capacy : " + ht.getCapacy());
     }
 }
